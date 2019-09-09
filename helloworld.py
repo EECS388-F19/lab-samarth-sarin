45e5e6cebcc9d2362c5f0d7a5ca4f9864fc8e676
samsarin <samsarin@umich.edu>
2019-09-09 15:43:17 -0400

    added file

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+import random
+
+print("Samarth Sarin")
+
+x = random.randint(0, 101)
+y = random.randint(0, 101)
+
+print(x)
+print(y)
+
+print("Sum ="),
+print(x + y)
+print("Average ="),
+print((x + y)/2)
+
+
+
+
+
